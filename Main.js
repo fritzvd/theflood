9849806937c808a7fffa11a1e57fec3b4ce631bb
fritzvd <f.vandeventer@gmail.com>
2014-11-30 21:49:38 +0100

    html5

--- Main.js
+++ Main.js
@@ -4915,9 +4915,10 @@ MainScene.prototype = $extend(com_haxepunk_Scene.prototype,{
 		if(!this.done) this.shouldSteal(); else this.doneTime += com_haxepunk_HXP.elapsed;
 		if(this.doneTime > 3) com_haxepunk_HXP.set_scene(new IntroScene());
 		this.soupBar.set_scaledWidth(this.cansOfSoup / 40 * 100 * this.scale | 0);
+		if(this.cansOfSoup == 40) this.done = true;
 		if(this.time > 0) this.time -= com_haxepunk_HXP.elapsed; else this.time = 0;
 		this.timeText.set_text(Std.string(Math.round(this.time * 100) / 100.0));
-		if(this.time <= 0 && !this.done) {
+		if(this.time <= 0 && !this.done || this.done) {
 			if(this.cansOfSoup == 40) {
 				this.done = true;
 				var wonText = new com_haxepunk_graphics_Text("Well Done",null,null,null,null,{ color : 218103807});
